3763739d4dd5a76597f74018400dee1431430f18
Chaw Thiri San <chawthiri177@gmail.com>
2025-04-26 14:05:14 +0900

    add new version

---
+++ api_based_detector/main.py
@@ -0,0 +1,26 @@
+# from img_processing import image_cropper
+from ocr import run_single_img_ocr
+import numpy as np
+import os 
+
+# TODO : Crop the sides of the image
+# TODO : Add barcode scanner
+# TODO : ADD bulk processing
+# TODO : add data base
+# TODO : add webcam processing
+# TODO : add autorotation
+# TODO : add parallel processing
+
+
+# -------------------------------------------------------- Single image test ----------------
+#img_path = r"Postal-DB-dataset\content\Postal-DB\cropped_dataset\testimg.jpg"
+#run_single_img_ocr(img_path=img_path)
+
+# --------------------------------------------------------- Folder of images test ------------
+img_folder = r"C:\Users\chawt\Desktop\inha_6_sem\Digital_Signal_Processing\Postal-DB-dataset\content\Postal-DB\cropped_dataset"
+
+# Loop through all files in the folder
+for filename in os.listdir(img_folder):
+    if filename.lower().endswith(('.jpg', '.jpeg', '.png')):
+        img_path = os.path.join(img_folder, filename)
+        run_single_img_ocr(img_path=img_path)
