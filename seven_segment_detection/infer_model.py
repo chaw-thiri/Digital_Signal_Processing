e8cdb2fc7151d3aeda9f3336b25b6ae92f59afa2
Chaw Thiri San <113085742+chaw-thiri@users.noreply.github.com>
2025-05-21 12:34:45 +0900

    Update infer_model.py

--- seven_segment_detection/infer_model.py
+++ seven_segment_detection/infer_model.py
@@ -1,14 +1,28 @@
 from inference_sdk import InferenceHTTPClient
+import requests
+from PIL import Image
+from io import BytesIO
+
+from IPython.display import display
+
+
+
 
 client = InferenceHTTPClient(
-    api_url="http://localhost:9001", # use local inference server
+    api_url="https://detect.roboflow.com",
     api_key="Dy8Ow3DQB5W7WtvMMTML"
 )
+
 
 result = client.run_workflow(
     workspace_name="ocr-pwajs",
     workflow_id="custom-workflow",
     images={
-        "image": "YOUR_IMAGE.jpg"
-    }
+        "image": "/content/20250403_115525.jpg" # sample image 
+    },
+    use_cache=True
 )
+
+print(result)
+
+
